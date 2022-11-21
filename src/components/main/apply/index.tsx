129445d0abe24b7653162c0d7690a332d86ddbd1
rlarlduf20 <kky8274@naver.com>
2022-11-22 02:50:06 +0900

    메인페이지-지원하기 섹션 완성

---
+++ src/components/main/apply/index.tsx
@@ -0,0 +1,124 @@
+import styled from "@emotion/styled";
+
+const ApplySection = styled.section`
+  background-color: ${(props) => props.theme.color.black};
+  .inner {
+    width: 1037px;
+    margin: 0 auto;
+    height: 500px;
+    .type {
+      color: ${(props) => props.theme.color.white};
+      font-size: 18px;
+      font-weight: 500;
+      padding: 13px 66px;
+      border: 3px solid;
+      border-radius: 50px;
+      margin-top: 22px;
+      display: inline-block;
+      border-color: ${(props) => props.theme.color.white};
+    }
+    .content {
+      color: ${(props) => props.theme.color.white};
+      font-size: 18px;
+      font-weight: 500;
+      display: inline-block;
+      margin-left: 39px;
+    }
+    .firstBox {
+      border-bottom: 1px solid white;
+      padding: 18px 0 43px 65px;
+      .qualify {
+        .f_type {
+          color: ${(props) => props.theme.color.black};
+          background-color: ${(props) => props.theme.color.white};
+          font-size: 18px;
+          font-weight: 700;
+          padding: 13px 66px;
+          border: 3px solid;
+          border-radius: 50px;
+          display: inline-block;
+        }
+      }
+    }
+    .secondBox {
+      padding: 4.5px 0 0 65px;
+      position: relative;
+      .wrap {
+        position: absolute;
+        top: 45px;
+        left: 270px;
+        .content {
+          display: block;
+          .subtype {
+            display: inline-block;
+            margin-bottom: 20px;
+          }
+          .subcontent {
+            display: inline-block;
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Apply = () => {
+  return (
+    <ApplySection>
+      <div className="inner">
+        <div className="firstBox">
+          <div className="qualify">
+            <div className="f_type">지원 자격</div>
+            <div className="content">
+              시각디자인학과 학부생 - 본전공생 (신입생, 재학생-2학년)
+            </div>
+          </div>
+          <div className="duration">
+            <div className="type">지원 기간</div>
+            <div className="content">
+              03.07 - 03.15{" "}
+              <span style={{ marginLeft: "25px" }}>23:00까지</span>
+            </div>
+          </div>
+          <div className="interview">
+            <div className="type">면접 일정</div>
+            <div className="content">
+              03.17{" "}
+              <span style={{ marginLeft: "10px", marginRight: "10px" }}>
+                19:00
+              </span>{" "}
+              ZOOM
+            </div>
+          </div>
+        </div>
+        <div className="secondBox">
+          <div className="type">
+            <span style={{ marginRight: "30px" }}>문</span>의
+          </div>
+          <div className="wrap">
+            <div className="content">
+              <p className="subtype" style={{ marginRight: "68px" }}>
+                오픈 카톡 문의 링크
+              </p>
+              <p className="subcontent">https://open.kakao.com/o/gOFvsm3d</p>
+            </div>
+            <div className="content">
+              <p className="subtype" style={{ marginRight: "177px" }}>
+                회장
+              </p>
+              <p className="subcontent">최예지 * 010 4189 0562</p>
+            </div>
+            <div className="content">
+              <p className="subtype" style={{ marginRight: "161px" }}>
+                부회장
+              </p>
+              <p className="subcontent">정다원 * 010 5131 6754</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ApplySection>
+  );
+};
+
+export default Apply;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,10 @@
-import styled from "@emotion/styled";
-
-const Title = styled.h2`
-  font-size: ${(props) => props.theme.font.size.h2}px;
-  text-align: center;
-  font-weight: 500;
-`;
+import Apply from "../components/main/apply";
 
 const Home = () => {
   return (
-    <Title>
-      스포카 서비스의 많은 부분은 오픈소스 기술로 이루어져 있습니다. 스포카는
-      오픈소스 기술을 이용하는 만큼 그 생태계에 기여합니다. 이러한 기여가
-      모일수록 오픈소스 커뮤니티가 건강해지며, 사용자도 함께 성장해 나갈
-      것입니다.
-    </Title>
+    <>
+      <Apply />
+    </>
   );
 };
 
