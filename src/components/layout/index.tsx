6762264fba96d3169f69f7c5100a6ba122e98aba
rlarlduf20 <kky8274@naver.com>
2022-11-22 15:10:48 +0900

    메인 margin 설정

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,7 +5,6 @@ import styled from "@emotion/styled";
 const Main = styled.main`
   margin-top: 105px;
   min-height: calc(100vh - 105px - 318px);
-  z-index: 999;
 `;
 
 const Layout = ({ children }: React.PropsWithChildren<{}>) => {
