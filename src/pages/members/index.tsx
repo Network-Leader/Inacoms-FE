9416bdef0e20f7ec19f14ec1b7a5d87def169c86
rlarlduf20 <kky8274@naver.com>
2023-01-16 15:19:41 +0900

    44-error

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -1,13 +1,17 @@
-import MembersMain from "../../components/members/main";
-import MembersTitle from "../../components/members/title";
+// import MembersMain from "../../components/members/main";
+// import MembersTitle from "../../components/members/title";
 
+// const Members = () => {
+//   return (
+//     <>
+//       <MembersTitle />
+//       <MembersMain />
+//     </>
+//   );
+// };
+
+// export default Members;
 const Members = () => {
-  return (
-    <>
-      <MembersTitle />
-      <MembersMain />
-    </>
-  );
+  return <></>;
 };
-
 export default Members;
